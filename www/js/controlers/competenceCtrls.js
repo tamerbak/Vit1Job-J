3d5e5b785142c83f197d8d1a16bf06f00e2f0193
Tamer <tamer.elbakkali@gmail.com>
2015-10-16 18:20:33 +0100

    regroupement de code tamer + omar

--- www/js/controlers/competenceCtrls.js
+++ www/js/controlers/competenceCtrls.js
@@ -4,8 +4,52 @@
 
 angular.module('competenceCtrls', ['ionic'])
 
-  .controller('competenceCtrl', function ($scope, $state) {
+  .controller('competenceCtrl', function ($scope) {
     $scope.njobyer = 1;
-    $scope.metier = "Métier";
+    //$scope.rangeValue = 0;
+    $scope.rangeModel = 0;
+    $scope.maitriseIcon = "icon ion-ios-rainy calm";
+    $scope.maitrise = "Débutant";
+
+    $scope.rangeChange = function(rangeModel) {
+      if (rangeModel <= 25 ){
+        $scope.maitrise = "Débutant";
+        $scope.maitriseIcon = "icon ion-ios-rainy calm";
+      }
+
+      else if (rangeModel > 25 && rangeModel <= 50 ) {
+        $scope.maitrise = 'Habitué';
+        $scope.maitriseIcon = "icon ion-ios-cloudy-outline calm";
+      }
+
+      else if (rangeModel > 50 && rangeModel <= 75 ){
+        $scope.maitrise = 'Confirmé';
+        $scope.maitriseIcon = "icon ion-ios-partlysunny-outline calm";
+      }
+      else if (rangeModel > 75 && rangeModel <= 100 ){
+        $scope.maitrise = 'Waouh!';
+        $scope.maitriseIcon = "icon ion-ios-sunny-outline calm";
+      }
+
+
+      //console.log ( $scope.rangeValue);
+      console.log ( rangeModel);
+
+      /*var deviceInfo = cordova.require("cordova/plugin/DeviceInformation");
+      deviceInfo.get(function(result) {
+        //fetch the device data
+        console.log("result = " + result);
+        $scope.maitrise = result;
+      }, function() {
+        console.log("error");
+        $scope.maitrise = "error";
+      });*/
+
+    };
+
+    $scope.addCompetence = function(){
+      $scope.njobyer = $scope.njobyer + 1;
+    }
+
 
   });
