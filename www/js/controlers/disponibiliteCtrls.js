737ed64218e0a8ef918b27f21e7f726c12e1b1e6
SoftBrain002 <m.aitbenadi@softbrain-it.com>
2015-12-07 23:43:44 +0000

    page disponibilite : fonction du bouton ajouter

--- www/js/controlers/disponibiliteCtrls.js
+++ www/js/controlers/disponibiliteCtrls.js
@@ -8,17 +8,27 @@ starter
 	.controller('disponibiliteCtrl', function ($scope, $rootScope, $cookieStore, $state,$ionicHistory, AuthentificatInServer,
 						Global, DataProvider, PullDataFromServer, PersistInServer,$ionicPopup){
 		// FORMULAIRE
-		$scope.formData={};
-    $scope.saveDisponibilite= function(){
+		$scope.formData={'jours':{'lundi':"",'mardi':"",'mercredi':"",'jeudi':"",'vendredi':"",'samedi':"",'dimanche':""}};
 
+    $scope.saveDisponibilite= function(){
+      var dateDebut= $scope.formData.dateDebut;
+      var dateFin= $scope.formData.dateFin;
+      var jamais = $scope.formData.jamais;
+      var jours= $scope.formData.jours;
+      var remuneration= $scope.formData.remuneration;
     };
-		$scope.initAll = function(){
+
+    $scope.initAll = function(){
 
 			// GET LIST
 			$scope.formData={
-
+            'jours':{'lundi':"",'mardi':"",'mercredi':"",'jeudi':"",'vendredi':"",'samedi':"",'dimanche':""},
+            "heures":[]
 				};
 
 		};
 
+    $scope.ajouterHeures= function(){
+      $scope.formData.heures.push({"heureDebut":$scope.formData.heureDebut,"heureFin":$scope.formData.heureFin});
+    };
   });
