122ca7a51e6195e3bc596e860f7a1d33f55a7eec
SoftBrain002 <m.aitbenadi@softbrain-it.com>
2015-12-08 18:05:00 +0000

    disponibilite modal

--- www/js/controlers/competenceCtrls.js
+++ www/js/controlers/competenceCtrls.js
@@ -5,7 +5,7 @@
 'use strict';
 starter
 
-	.controller('competenceCtrl', function ($scope, $rootScope, $cookieStore, $state,$ionicHistory, x2js, AuthentificatInServer,
+	.controller('competenceCtrl', function ($scope, $rootScope, $cookieStore, $state,$ionicHistory,$ionicModal, x2js, AuthentificatInServer,
 						Global, DataProvider, PullDataFromServer, PersistInServer, LoadList, formatString, UploadFile,$ionicPopup){
 		// FORMULAIRE
 		$scope.formData={};
@@ -162,6 +162,15 @@ starter
 			console.log("All Jobyers : "+JSON.stringify($rootScope.jobyers));
 		};
 
+    $scope.addDispo = function(){
+      $ionicModal.fromTemplateUrl('templates/disponibiliteCompetenceModal.html', {
+        scope: $scope
+      }).then(function(modal) {
+        $scope.modal = modal;
+        $scope.modal.show();
+      });
+    };
+
 		$scope.addJobyer = function(){
 			// VALIDATION DES CHAMPS
 			var metier=null, job=null;
@@ -214,7 +223,7 @@ starter
         $rootScope.jobyers[Number(jobyerCurrent.indice)-1].maitriseLangue=maitriseLangue;
         $rootScope.jobyers[Number(jobyerCurrent.indice)-1].maitriseLangueIcon=maitriseLangueIcon;
 				// UPDATE IN VIEW
-				$scope.formData.currentFeuille=jobyerCurrent.indice+1;;
+				$scope.formData.currentFeuille=jobyerCurrent.indice+1;
 				$scope.formData.allFeuilles=$rootScope.jobyers.length;
 
 				console.log("Old Jobyer : "+JSON.stringify($rootScope.jobyers[Number(jobyerCurrent.indice)-1]));
@@ -232,7 +241,6 @@ starter
         $rootScope.jobyerCurrent['maitriseLangue']=maitriseLangue;
         $rootScope.jobyerCurrent['maitriseLangueIcon']=maitriseLangueIcon;
 				$rootScope.jobyers.push($rootScope.jobyerCurrent); // AJOUTE ///MAIS C'EST UNDIFINED
-
 				console.log("New Jobyer : "+JSON.stringify($rootScope.jobyers[Number($rootScope.jobyerCurrent.indice)-1]));
 
 				// REFRESH BUTTONS
@@ -668,6 +676,7 @@ starter
 				// SHOW MODAL
 				//Global.showAlertPassword("Merci! Vos Offres sont été bien publiés.");
 				// REDIRECTION VERS home
+        //$state.go("disponibilite");
         var myPopup = $ionicPopup.show({
           template: "Votre compte a été crée avec succés <br>",
           title: "<div class='vimgBar'><img src='img/vit1job-mini2.png'></div>",
@@ -678,8 +687,7 @@ starter
             }
           ]
         });
-        $state.go("disponibilite");
-				//$state.go("app");
+				$state.go("app");
 			}
 		};
 	  $scope.backbutton = function()
@@ -718,6 +726,110 @@ starter
 		$scope.isValid=function(field){
 
 			return !isNaN(Number(field));
-		}
+		};
 
+    $scope.formData.jours=DataProvider.getDays();
+    console.log($scope.formData.jours.length);
+    $scope.saveDisponibilite= function(){
+      var dateDebut= $scope.formData.dateDebut;
+      var dateFin= $scope.formData.dateFin;
+      var jamais = $scope.formData.jamais;
+      var jours= $scope.formData.jours;
+      var remuneration= $scope.formData.remuneration;
+      var heures=$scope.formData.heures;
+      console.log(JSON.stringify($scope.formData));
+      if(dateDebut && (dateFin || jamais) && jours && remuneration && !heures.$isEmpty()) {
+
+        // RECUPERATION CONNEXION
+        var connexion=$cookieStore.get('connexion');
+        // RECUPERATION JOBEYER ID
+        var jobeyeId=connexion.jobeyeId;
+        // RECUPERATION SESSION ID
+        sessionId=$cookieStore.get('sessionID');
+        var hasSessionID=0;
+        if(sessionId)
+          hasSessionID=1;
+        else{	// RECUPERATION SESSION-ID
+          AuthentificatInServer.getSessionId()
+            .success(function (response){
+              var jsonResp = x2js.xml_str2json(response);
+              var jsonText = JSON.stringify (jsonResp);
+              jsonText = jsonText.replace("fr.protogen.connector.model.AmanToken","amanToken");
+              jsonResp = JSON.parse(jsonText);
+
+              // GET SESSION ID
+              sessionId = jsonResp.amanToken.sessionId;
+              console.log("New sessionId : "+sessionId);
+              $cookieStore.put('sessionID', sessionId);
+              hasSessionID=1;
+            })
+            .error(function (err){
+              console.log("error : Récuperation Session ID");
+              console.log("error In saveJobyers: "+err);
+            });
+        }
+        var heure_d=heures[0].heureDebut;
+        var heure_f=heures[0].heureFin;
+        if(hasSessionID){
+          PersistInServer.persistInDisponibilite(jourId, heure_d, heure_f, sessionId, offreId)
+            .then(
+              function (response){
+                console.log("response : "+JSON.stringify(response));
+
+                var myPopup = $ionicPopup.show({
+                  template: "Votre compte a été crée avec succés <br>",
+                  title: "<div class='vimgBar'><img src='img/vit1job-mini2.png'></div>",
+                  buttons: [
+                    {
+                      text: '<b>OK</b>',
+                      type: 'button-calm'
+                    }
+                  ]
+                });
+                $state.go("app");
+              },function (err){
+                console.log("error : insertion DATA");
+                console.log("error In PullDataFromServer.pullDATA: "+err);
+              }
+            );
+        }
+
+      }else{
+        Global.showAlertValidation("Veuillez saisir tous les champs");
+      }
+
+    };
+
+    $scope.initModalAll = function(){
+
+      // GET LIST
+      $scope.formData={
+        "heures":[]
+      };
+      $scope.formData.jours=DataProvider.getDays();
+    };
+
+    $scope.ajouterHeures= function(){
+
+      var hdebut=$scope.formData.heureDebut;
+      var hfin=$scope.formData.heureFin;
+      var mdebut=$scope.formData.minDebut;
+      var mfin=$scope.formData.minFin;
+
+      if(hdebut!=undefined && hfin!=undefined && mdebut!=undefined && mfin!=undefined) {
+        if(hfin > hdebut)
+          $scope.formData.heures.push({"heureDebut": hdebut+"h"+mdebut+"min", "heureFin": hfin+"h"+mfin+"min"});
+        else
+          Global.showAlertValidation("L'heure de fin doit être supérieur.");
+      }else{
+        Global.showAlertValidation("Veuillez saisir une heure de début et une heure de fin.");
+      }
+    };
+
+    $scope.supprimerHeures= function(){
+
+      if( $scope.formData.heures.length!=0){
+        $scope.formData.heures.pop();
+      }
+    };
   });
