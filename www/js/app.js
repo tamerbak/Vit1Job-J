231b56eba2edb4fc78d70cf4c13a5aba0cc525c4
SoftBrain002 <m.aitbenadi@softbrain-it.com>
2016-01-05 13:53:25 +0000

    #77505619240201 page offres et ses onglets

--- www/js/app.js
+++ www/js/app.js
@@ -146,3 +146,28 @@ starter.directive('googleplace', function() {
     }
   };
 });
+starter.directive('groupedRadio', function() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    scope: {
+      model: '=ngModel',
+      value: '=groupedRadio'
+    },
+    link: function(scope, element, attrs, ngModelCtrl) {
+      element.addClass('button');
+      element.on('click', function(e) {
+        scope.$apply(function() {
+          ngModelCtrl.$setViewValue(scope.value);
+        });
+      });
+
+      scope.$watch('model', function(newVal) {
+        element.removeClass('button-green');
+        if (newVal === scope.value) {
+          element.addClass('button-green');
+        }
+      });
+    }
+  };
+});
