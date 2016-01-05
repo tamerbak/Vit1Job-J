d0ac66148a40a0b938845e8992d6cb1e8ee84e4a
SoftBrain002 <m.aitbenadi@softbrain-it.com>
2016-01-05 09:47:59 +0000

    page siège sociale

--- www/js/app.js
+++ www/js/app.js
@@ -126,3 +126,23 @@ var services = angular.module('Services', []);
 function isEmpty(str) {
 	return (!str || 0 === str.length || typeof str === 'undefined' || str === null);
 }
+starter.directive('googleplace', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attrs, model) {
+      var options = {
+        types: [],
+        componentRestrictions: {
+          country : 'FR'
+        }
+      };
+      scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+
+      google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+        scope.$apply(function() {
+          model.$setViewValue(element.val());
+        });
+      });
+    }
+  };
+});
